1bee2ec523943bbc4a1c538bb48cb743a39a3a8c
Ilke Yucel <ilke.yucel@morhipo.com>
2021-04-20 22:45:08 +0300

    - Initial commit.

---
+++ app/src/main/java/com/ilkeyucel/btmobilapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.ilkeyucel.btmobilapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button gotoButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gotoButton = (Button)findViewById(R.id.button3);
+
+        gotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("login here");
+                Intent intent = new Intent(v.getContext(), SignUpActivity.class);
+                v.getContext().startActivity(intent);
+            }
+        });
+
+    }
+}
